b3caeddee79181c5ec3249a90e01c4897a3bd540
Thomas Kvalnes <thomas.kvalnes@nina.no>
2024-09-27 16:35:17 +0200

    Removed old tests

--- tests/testthat/test-list_gts_parameters.R
+++
@@ -1,4 +0,0 @@
-test_that("download of data frame", {
-  l <- list_gts_parameters()
-  expect_equal(is.data.frame(l), TRUE)
-})
--- tests/testthat/test-sf_to_json.R
+++
@@ -1,6 +0,0 @@
-test_that("conversion works", {
-  library(sf)
-  pol <- st_as_sf(st_as_sfc(st_bbox(st_as_sf(x = data.frame(x = c(58200.16, 74224.55, 58200.16, 74224.55), y = c(6708224.65, 6708224.65, 6715024.86, 6715024.86)), coords = c("x", "y"), crs = 25833))))
-  exp_res <- "{'rings':[[[58200.16,6708224.65],[74224.55,6708224.65],[74224.55,6715024.86],[58200.16,6715024.86],[58200.16,6708224.65]]],'spatialReference':{'wkid':25833}}"
-  expect_equal(sf_to_json(pol), exp_res)
-})
